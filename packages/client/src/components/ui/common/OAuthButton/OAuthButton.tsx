00f5d7b601f7984d2d6a71c5bc6e357dcf1c3fe7
Ales Pavlov <pavlov.a@ad.eyetronic.ru>
2024-07-31 23:03:22 +0300

    lvl-56: add OAuthButton component

---
+++ packages/client/src/components/ui/common/OAuthButton/OAuthButton.tsx
@@ -0,0 +1,19 @@
+import { Button, message } from 'antd'
+import yandexOauthLogo from '@/assets/yandex-oauth.svg'
+import oAuthService from '@/utils/oAuthService'
+import styles from './OAuthButton.module.scss'
+
+export const OAuthButton = () => {
+  const handleOAuthLogin = async () => {
+    const result = await oAuthService.redirectToOAuth()
+    if (result.error) {
+      message.error(`Login error: ${result.error}`)
+    }
+  }
+
+  return (
+    <Button type='primary' size='large' htmlType='button' onClick={handleOAuthLogin} className={styles.button}>
+      <img src={yandexOauthLogo} alt='Логотип Яндекс' className={styles.yandexLogo} />
+    </Button>
+  )
+}
---
+++ packages/client/src/components/ui/common/OAuthButton/index.ts
@@ -0,0 +1,1 @@
+export { OAuthButton } from './OAuthButton'
