c91a70a1a6ebf450095173407e998a93c3f9ab6a
Igor Yamaleev <jamigo@ya.ru>
2024-08-14 09:21:26 +0400

    work on topic's reactions started

--- packages/client/src/components/ui/Forum/ForumTopicList/ForumTopicList.tsx
+++ packages/client/src/components/ui/Forum/ForumTopicList/ForumTopicList.tsx
@@ -3,11 +3,13 @@ import { Topic } from 'types/forum'
 import { List, Avatar } from 'antd'
 import { useAppSelector } from '@/hooks/reduxHooks'
 import { selectTopicList } from '@/store/slices/forumTopic/forumTopicSelector'
+import EmojiPicker, { Theme } from 'emoji-picker-react'
 import { GetTopicDescription } from './helpers'
 import styles from './ForumTopicList.module.scss'
 
 export const ForumTopicList = () => {
   const topicList = useAppSelector(selectTopicList)
+  const handleReaction = () => {}
 
   return (
     <div className={styles.forumTopics}>
@@ -19,13 +21,16 @@ export const ForumTopicList = () => {
         }}
         className={styles.list}
         renderItem={(topic: Topic) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={topic.author.avatar} />}
-              title={<Link to={`/forum/topic/${topic.id}`}>{topic.title}</Link>}
-              description={GetTopicDescription(topic)}
-            />
-          </List.Item>
+          <>
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src={topic.author.avatar} />}
+                title={<Link to={`/forum/topic/${topic.id}`}>{topic.title}</Link>}
+                description={GetTopicDescription(topic)}
+              />
+            </List.Item>
+            <EmojiPicker reactionsDefaultOpen onReactionClick={handleReaction} theme={Theme.DARK} />
+          </>
         )}
       />
     </div>
