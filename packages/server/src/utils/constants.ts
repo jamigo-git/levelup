934d7a164a0083f69c59916ee7db0927ccaa6671
Igor Yamaleev <jamigo@ya.ru>
2024-09-04 02:42:47 +0400

    server-postgresql connection fix 7

--- packages/server/src/utils/constants.ts
+++ packages/server/src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const EXTERNAL_API_URL = 'https://ya-praktikum.tech/api/v2'
 
 const clientPort = Number(process.env.CLIENT_PORT) || 3000
-export const CORS_ORIGINS = [`http://levelup-towerdefense-39.ya-praktikum.tech:${clientPort}`]
+export const CORS_ORIGINS = [`http://prakticum-server:${clientPort}`]
