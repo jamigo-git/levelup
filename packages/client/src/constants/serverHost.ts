b9b1b08e3d1198dad473e9bf435a4f6dc2058034
Denis Evgenev <d.a.evgenyev@gmail.com>
2024-09-07 11:58:24 +0300

    LVL-69-fix6: Написать конфиг nginx

--- packages/client/src/constants/serverHost.ts
+++ packages/client/src/constants/serverHost.ts
@@ -1,4 +1,4 @@
 import '../client.d'
 
 export const SERVER_HOST = typeof window === 'undefined' ? __INTERNAL_SERVER_URL__ : __EXTERNAL_SERVER_URL__
-export const API_HOST = 'http://prakticum-server:3001'
+export const API_HOST = ''
